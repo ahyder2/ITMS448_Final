e9bbe648378ae440a23e6d1555c3f746a54eee18
ahyder2 <54422372+ahyder2@users.noreply.github.com>
2020-11-25 17:59:02 -0600

    Test to verufy scraper functionality
    Makes sure scraper is working by utilizing basic functionailty. This will be itterated upon for further. Here is the program output I got:
    
    Enter subreddit to scrape: VALORANT
    jrowb2
    jkd9ee
    k0tzo4
    k0ygy0
    k0k12b

--- Reddit_Scraper.py
+++ Reddit_Scraper.py
@@ -3,12 +3,21 @@
 
 import praw
 
-#def ScrapeReddit(subreddit):
+def ScrapeReddit(subreddit): #function for scraping a desired subreddit. Will be utilized in the GUI when the Scrape Reddit option is selected. subreddit parameter is the user input for the sub that will be scraped
 
-reddit = praw.Reddit (client_id = "hsPskyIcTh7EqA" ,
-                client_secret = "GniFoEIRe3W2RLpA8qWrOVt4b8xUOg",
-                username = "448Scraper",
-                password = "Pass448!",
-                user_agent = "ITMS 448 Scraper")
+    reddit = praw.Reddit (client_id = "hsPskyIcTh7EqA" ,
+                    client_secret = "GniFoEIRe3W2RLpA8qWrOVt4b8xUOg",
+                    username = "448Scraper",
+                    password = "Pass448!",
+                    user_agent = "ITMS 448 Scraper")
 
-print(reddit.user.me())
+    sub=reddit.subreddit(subreddit) #accesses specified subreddit and stores it in variable sub
+    sub_hot = sub.hot(limit = 5) #goes throught the 5 hottest posts in specifed sub reddit that will be scraped
+
+    for submission in sub_hot: #prints the post IDs for the 5 hottest posts in the specified sub
+        print(submission) #this will be updated for more sophisticated functionality. currently here for testing purposes
+
+#anything below this line is for testing purposes. the function will be implemented in the GUI and not the class iteself
+scansub = input("Enter subreddit to scrape: ")
+ScrapeReddit(scansub)
+
