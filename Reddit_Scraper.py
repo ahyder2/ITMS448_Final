2498a72ebedc1367b989cd741d0d668032493fe4
ahyder2 <54422372+ahyder2@users.noreply.github.com>
2020-11-25 17:21:20 -0600

    reddit instance created - just checking to make sure praw is authenticating as itnended

--- Reddit_Scraper.py
+++ Reddit_Scraper.py
@@ -1,2 +1,14 @@
 #Reddit Scraper
 #Programmed by Asim Hyder
+
+import praw
+
+#def ScrapeReddit(subreddit):
+
+reddit = praw.Reddit (client_id = "hsPskyIcTh7EqA" ,
+                client_secret = "GniFoEIRe3W2RLpA8qWrOVt4b8xUOg",
+                username = "448Scraper",
+                password = "Pass448!",
+                user_agent = "ITMS 448 Scraper")
+
+print(reddit.user.me())
