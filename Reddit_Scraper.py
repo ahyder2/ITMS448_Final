01d15dc25e8ab5659acbf1e2dd4b32d3142c10d9
ahyder2 <54422372+ahyder2@users.noreply.github.com>
2020-11-25 19:25:19 -0600

    Added fields to scrape and displayed them
    Used the NumPy library to solve the error from before. Most likely will not really use it in the future as the Pandas library will be used for data anamysis. here is my output.
    
    Enter subreddit to scrape: VALORANT
    {'post_title': ["The coolest Cypher cam you'll never use", 'Icebox cam for B and to watch rotates', 'C9 Relyks explains the run and gun accuracy problem/deadzone', 'Haven boost spot to use when feeling sneaky', 'Rank allocation has shifted? 50% of players are now in Iron and Bronze apparently.'], 'post_body': ['', '', '', '', "So a friend of mine drew my attention to this the other day: https://www.valking.gg/ranked-distribution\n\nThe current rank distribution is now heavily stacked towards the lowest ranks with roughly 70 percent of the playerbase in Silver or below. Iron is now the biggest bracket.\n\nThis surprised me because I specifically remember looking at the rank allocation a couple months ago and Gold used to be by far the biggest bracket. Or am I going crazy? \n\nEdit: For reference someone posted [this](https://images.squarespace-cdn.com/content/v1/59af2189c534a58c97bd63b3/1599231342643-JKU7YWMGGG6F8W74CNW7/ke17ZwdGBToddI8pDm48kFVenJculfcaGOEfNaBtEp0UqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKctUiQ2bslD1dqUvrw8zBOsoKRrG16EUx0KQZvqXsvLnXa71UdJmZ9gFFvNfnS4Xbg/Valorant+Rank+Distribution+August+2020.jpg?format=1500w) distribution from August 2020.\n\nIt seems like there's has been a shift but I'm not sure when it happened. And I feel like this would also explain why a lot of people are reporting to have deranked hard in this new act and are now struggling in high Bronze or low Silver when previously they were entrenched in Gold.\n\nThoughts?"], 'comment_num': [35, 24, 26, 94, 343], 'karma': [2235, 1835, 244, 6436, 976], 'link': ['https://v.redd.it/hgbc0o0knf161', 'https://v.redd.it/kbzuhfs0le161', 'https://www.youtube.com/watch?v=q7GICZIU-_0', 'https://v.redd.it/lm6zkdsvxa161', 'https://www.reddit.com/r/VALORANT/comments/k0p1xc/rank_allocation_has_shifted_50_of_players_are_now/'], 'post_date': [1606359802.0, 1606346319.0, 1606366224.0, 1606302908.0, 1606325254.0]}

--- Reddit_Scraper.py
+++ Reddit_Scraper.py
@@ -2,6 +2,8 @@
 #Programmed by Asim Hyder
 
 import praw
+import numpy as np
+import pandas as pd
 
 def ScrapeReddit(subreddit): #function for scraping a desired subreddit. Will be utilized in the GUI when the Scrape Reddit option is selected. subreddit parameter is the user input for the sub that will be scraped
 
@@ -12,9 +14,9 @@ def ScrapeReddit(subreddit): #function for scraping a desired subreddit. Will be
                     user_agent = "ITMS 448 Scraper")
 
     sub=reddit.subreddit(subreddit) #accesses specified subreddit and stores it in variable sub
-    sub_hot = sub.hot(limit = 5) #goes throught the 5 hottest posts in specifed sub reddit that will be scraped
+    sub_hot = sub.hot(limit = 7) #goes throught the 5 hottest posts in specifed sub reddit that will be scraped
     #array to store the scraped values of each post
-    posts_array = {"post_title":[], #title of post
+    posts_array = {"post_title":[],  #title of post
                     "post_body":[], #body of post
                     "comment_num":[], #number of comments
                     "karma":[], #post karma
@@ -24,17 +26,13 @@ def ScrapeReddit(subreddit): #function for scraping a desired subreddit. Will be
     for submission in sub_hot: #prints the post IDs for the 5 hottest posts in the specified sub
         if not submission.stickied: #doesn't scrape sitckied posts
             posts_array["post_title"].append(submission.title)
-            print(posts_array[0])
             posts_array["post_body"].append(submission.selftext)
-            print(posts_array[1])
             posts_array["comment_num"].append(submission.num_comments)
-            print(posts_array[2])
             posts_array["karma"].append(submission.score)
-            print(posts_array[3])
             posts_array["link"].append(submission.url)
-            print(posts_array[4])
             posts_array["post_date"].append(submission.created)
-            print(posts_array[5])
+    np.set_printoptions(threshold=np.inf)
+    print(posts_array)
 
 #anything below this line is for testing purposes. the function will be implemented in the GUI and not the class iteself
 scansub = input("Enter subreddit to scrape: ")
