4a9f112597c03f661d805bf63392a0192cb5a40a
ahyder2 <54422372+ahyder2@users.noreply.github.com>
2020-11-25 18:50:35 -0600

    Gathering more specific info
    This needs debugging as the array values are not being printed properly

--- Reddit_Scraper.py
+++ Reddit_Scraper.py
@@ -13,9 +13,28 @@ def ScrapeReddit(subreddit): #function for scraping a desired subreddit. Will be
 
     sub=reddit.subreddit(subreddit) #accesses specified subreddit and stores it in variable sub
     sub_hot = sub.hot(limit = 5) #goes throught the 5 hottest posts in specifed sub reddit that will be scraped
+    #array to store the scraped values of each post
+    posts_array = {"post_title":[], #title of post
+                    "post_body":[], #body of post
+                    "comment_num":[], #number of comments
+                    "karma":[], #post karma
+                    "link":[], #post url
+                    "post_date":[]} #date posted
 
     for submission in sub_hot: #prints the post IDs for the 5 hottest posts in the specified sub
-        print(submission) #this will be updated for more sophisticated functionality. currently here for testing purposes
+        if not submission.stickied: #doesn't scrape sitckied posts
+            posts_array["post_title"].append(submission.title)
+            print(posts_array[0])
+            posts_array["post_body"].append(submission.selftext)
+            print(posts_array[1])
+            posts_array["comment_num"].append(submission.num_comments)
+            print(posts_array[2])
+            posts_array["karma"].append(submission.score)
+            print(posts_array[3])
+            posts_array["link"].append(submission.url)
+            print(posts_array[4])
+            posts_array["post_date"].append(submission.created)
+            print(posts_array[5])
 
 #anything below this line is for testing purposes. the function will be implemented in the GUI and not the class iteself
 scansub = input("Enter subreddit to scrape: ")
